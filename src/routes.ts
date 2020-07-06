675427c211ccb83649172b3ad47e599231a4bbfe
Rafael Bozzetti <rafaelbozzetti@gmail.com>
2020-07-06 18:07:57 -0300

    task: isolate routes on file

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,17 @@
-import { Response, Request } from 'express';
+import { Response, Request, request } from 'express';
 import LoadApiContacts from './services/LoadApiContacts';
+import LoadCsvService from './services/LoadCsvService';
 
 export function contacts(request: Request, response: Response) {
 
     const loadApiContacts = new LoadApiContacts();
     const contacts = loadApiContacts.execute();
-
-    console.log(contacts);
-
     return response.json(contacts);
+};
+
+export function populate(request: Request, response: Response) {
+
+    const loadCsvService = new LoadCsvService();
+    const data = loadCsvService.execute({fileName: 'contacts.csv'});
+    return response.json(data);
 };
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,14 @@
 import express, { Response, Request } from 'express';
-import { contacts } from './routes';
-
-import UpdateUserAvatarService from './services/LoadCsvService';
+import { contacts, populate } from './routes';
 
 const app = express();
+
 app.use(express.json());
 
-app.get('/', async function(request: Request, response : Response ) {
-  const service = new UpdateUserAvatarService();
-  const data = await service.execute({fileName: 'contacts.csv'});
 
-  //console.log(data);
-  return response.json(data);
-});
-
+app.get('/', populate);
 app.get('/contacts', contacts);
+
 
 app.listen(3333, () => {
   console.log('🤖️ Axur server started');
