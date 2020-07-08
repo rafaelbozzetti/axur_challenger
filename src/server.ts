263e120e0da575a2dde7c17a922b2a9f423a9f36
Rafael Bozzetti <rafaelbozzetti@gmail.com>
2020-07-07 21:44:04 -0300

    task: define start routine and one endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,31 @@
 import express, { Response, Request } from 'express';
-import { contacts, populate } from './routes';
+import { contacts } from './routes';
+import VerifyContactList from './services/VerifyContactList';
+import LoadCsvService from './services/LoadCsvService';
 
 const app = express();
 
 app.use(express.json());
 
 
-app.get('/', populate);
-app.get('/contacts', contacts);
+const contactList = new VerifyContactList();
+contactList.execute().then(function(data) {
+ 
+  const { type, listId } = data;
 
+  if(type == 'new') {
+    const loadList = new LoadCsvService();
+    loadList.execute({listId:listId});
+  }
+
+});
+
+
+
+
+
+//app.get('/contacts', contacts);
 
 app.listen(3333, () => {
-  console.log('🤖️ Axur server started');
+  console.log('Axur server started');
 });
